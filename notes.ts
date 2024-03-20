189d8f7c4a8b54c937e4ae59f88b8e80840bdaff
AmTripathiDev <harshita2000singh021@gmail.com>
2024-03-20 22:20:28 +0530

    CI/CD Deployment

---
+++ notes.ts
@@ -0,0 +1,13 @@
+// you have also a machine and you will rent a
+// machine by aws server the one and main difference is that it has a public IP address
+
+// press a launch insatance means buy a machine
+
+// https runs on 443
+// http runs on 80
+// inbound rules ->
+// outbound rules -> you want to buy a machine that does not have access to intenet or can not access certain websites
+// ec2-52-66-255-97.ap-south-1.compute.amazonaws.com	
+// escape -> : -> wq
+// .json/yaml/xml => all are the way to represent data in different different format
+//ssh -i new-deploy-key-todo-app.pem  ubuntu@ec2-13-233-131-221.ap-south-1.compute.amazonaws.com
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+console.log("change ");
+
+// mongoose.connect(
+//   "mongodb://dbUser:dbUser@ac-zcx3rpl-shard-00-00.ltxiedh.mongodb.net:27017,ac-zcx3rpl-shard-00-01.ltxiedh.mongodb.net:27017,ac-zcx3rpl-shard-00-02.ltxiedh.mongodb.net:27017/?ssl=true&replicaSet=atlas-nujjw6-shard-0&authSource=admin&retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: "courses",
+//   }
+// );
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
