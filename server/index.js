606d842887c75d264163fa65a3e8f914c791daad
AmTripathiDev <abhitripathi9800q@gmail.com>
2024-03-31 22:19:10 +0530

    added some changes in index file

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use("/user", userRouter);
 //   }
 // );
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(3000, () => console.log("Server is running on port 3001"))
 
 // Dockerize 
 // Aws Deployment 
