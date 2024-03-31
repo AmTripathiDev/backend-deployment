e61f35a0e27cdddedb865469a0389316661de25e
AmTripathiDev <abhitripathi9800q@gmail.com>
2024-03-31 23:31:29 +0530

    bin file change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.12.0/bin
 
 cd backend-deployment
 git pull origin master
