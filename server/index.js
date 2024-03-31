913ea8e2409bac1585cde2c3e37513de815ee494
AmTripathiDev <abhitripathi9800q@gmail.com>
2024-03-31 23:41:07 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -19,4 +19,4 @@ app.use("/user", userRouter);
 //   }
 // );
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(3000, () => console.log("Server is running on port 3001"))
