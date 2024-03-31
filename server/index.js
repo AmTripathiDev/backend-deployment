c96d027f8f51302d47b340125596e5a952db6f9c
AmTripathiDev <abhitripathi9800q@gmail.com>
2024-03-31 14:29:09 +0530

    added some changes

--- server/index.js
+++ server/index.js
@@ -5,16 +5,10 @@ const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
-
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-console.log("change ");
 
 // mongoose.connect(
 //   "mongodb://dbUser:dbUser@ac-zcx3rpl-shard-00-00.ltxiedh.mongodb.net:27017,ac-zcx3rpl-shard-00-01.ltxiedh.mongodb.net:27017,ac-zcx3rpl-shard-00-02.ltxiedh.mongodb.net:27017/?ssl=true&replicaSet=atlas-nujjw6-shard-0&authSource=admin&retryWrites=true&w=majority",
@@ -25,4 +19,8 @@ console.log("change ");
 //   }
 // );
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3000, () => console.log("Server is running on port 3000"))
+
+// Dockerize 
+// Aws Deployment 
+// Volumes and Networks
